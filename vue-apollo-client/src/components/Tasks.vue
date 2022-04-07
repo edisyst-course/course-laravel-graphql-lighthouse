7f534d6f4a8ddd4c7709a6e9c1ffd45c7770eb4c
edisyst <editestar@gmail.com>
2022-04-07 22:27:25 +0200

    DONE: CRUD Tasks

--- vue-apollo-client/src/components/Tasks.vue
+++ vue-apollo-client/src/components/Tasks.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
+        <div v-show="open_edit_form">
+            <input type="text" v-model="title_edit">
+            <button @click="save_edit_task">Update</button>
+        </div>
+
         <ul>
             <li v-for="task in tasks" :key="task.id">
                 {{ task.title }}
+                <button @click="edit_task(task.id, task.title)">Edit</button>
+                <button @click="delete_task(task.id)">Delete</button>
             </li>
         </ul>
         <button @click="open_form=true">Add a task</button>
@@ -17,13 +24,18 @@
 <script>
 import TASKS_QUERY from "@/graphql/Tasks.gql";
 import CREATE_TASK_MUTATION from "@/graphql/CreateTask.gql";
+import DELETE_TASK_MUTATION from "@/graphql/DeleteTask.gql";
+import UPDATE_TASK_MUTATION from "@/graphql/UpdateTask.gql";
 
 export default {
     data() {
         return {
             tasks: null,
             title: '',
-            open_form: false
+            title_edit: '',
+            open_form: false,
+            open_edit_form: false,
+            id_edit: null
         };
     },
 
@@ -40,7 +52,40 @@ export default {
                 this.open_form = false
                 this.$apollo.queries.tasks.refetch()
             })
-        }
+        },
+
+        edit_task(id, title) {
+            this.open_edit_form = true
+            this.id_edit = id
+            this.title_edit = title
+        },
+
+        save_edit_task() {
+            this.$apollo.mutate({
+                mutation: UPDATE_TASK_MUTATION,
+                variables: {
+                    id: this.id_edit,
+                    title: this.title_edit,
+                }
+            }).then(() => {
+                this.id_edit = null
+                this.title_edit = ''
+                this.open_edit_form = false
+                this.$apollo.queries.tasks.refetch()
+            })
+        },
+
+        delete_task(id) {
+            this.$apollo.mutate({
+                mutation: DELETE_TASK_MUTATION,
+                variables: {
+                    id: id,
+                }
+            }).then(() => {
+                this.$apollo.queries.tasks.refetch()
+            })
+        },
+
     },
 
     apollo: {
